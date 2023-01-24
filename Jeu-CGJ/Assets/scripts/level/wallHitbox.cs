28a14a8c0893c355d8af2c31482da1f179c88399
Odilon <odilon.vidal@icloud.com>
2023-01-24 16:46:51 +0100

    mob ajouté

--- Jeu-CGJ/Assets/scripts/level/wallHitbox.cs
+++ Jeu-CGJ/Assets/scripts/level/wallHitbox.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class level : MonoBehaviour
+public class wallHitbox : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,13 @@ public class level : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            transform.parent.GetComponent<mobMovementScript>().Flip();
+        }
     }
 }
