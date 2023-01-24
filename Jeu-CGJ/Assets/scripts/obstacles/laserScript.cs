bef5d6b2c0d173557a078de794323baf76bfe968
fradetn <nael.fradet@gmail.com>
2023-01-24 13:45:21 +0100

    commit laser

---
+++ Jeu-CGJ/Assets/scripts/obstacles/laserScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserScript : MonoBehaviour
+{
+    public GameObject player;
+
+    [SerializeField] private float defRayDistance = 100;
+    public Transform laserPoint;
+    public LineRenderer lineRenderer;
+    Transform m_transform; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_transform = GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        if(Physics2D.Raycast(m_transform.position, Vector2.down))
+        {
+
+            RaycastHit2D _hit = Physics2D.Raycast(laserPoint.position, Vector2.down);
+            Draw2DRay(laserPoint.position, _hit.point);
+            
+        }
+        else 
+        {
+            Draw2DRay(laserPoint.position, Vector2.down * defRayDistance);
+        }
+    }
+
+    void Draw2DRay(Vector2 startPos, Vector2 endPos)
+    {
+        lineRenderer.SetPosition(0, startPos);
+        lineRenderer.SetPosition(1, endPos);
+    }
+}
