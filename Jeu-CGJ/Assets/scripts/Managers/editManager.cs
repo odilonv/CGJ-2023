f74a6036e829188c99ebb56e2ff558da6461a90d
Esclapez Loïc <loicesclapez@gmail.com>
2023-01-24 14:07:10 +0100

    depot

---
+++ Jeu-CGJ/Assets/scripts/Managers/editManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class editManager : MonoBehaviour
+{
+    public static int editState;
+    public static bool editMode;
+}
---
+++ Jeu-CGJ/Assets/scripts/stateDisplayScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stateDisplayScript : MonoBehaviour
+{
+    public parametrableScript param;
+
+    void Start()
+    {
+        param = transform.parent.GetComponent<parametrableScript>();
+    }
+
+    void Update(){
+        foreach (Transform Child in transform) 
+        {
+            Child.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        if(param.mode != -2)
+        {
+            transform.GetChild(param.mode+1).GetComponent<SpriteRenderer>().enabled = true;
+        }
+        
+        
+    }
+}
