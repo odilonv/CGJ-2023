a4119f3a79680f9ed13dea45cf7cabc0df39d79a
iguerchal <louis.leborgne2@gmail.com>
2023-01-25 03:00:36 +0100

    elevator function

--- Jeu-CGJ/Assets/scripts/level/obstacles/ElevatorScript.cs
+++ Jeu-CGJ/Assets/scripts/level/obstacles/ElevatorScript.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class ElevatorScript : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public parametrableScript param;
+
 
     // Update is called once per frame
     void Update()
@@ -18,12 +15,60 @@ public class ElevatorScript : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 30);
+        start(collision);
     }
     void OnTriggerStay2D(Collider2D collision)
     {
-        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 30);  
+        start(collision);  
         
+    }
+
+
+    public void play(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 30);
+    }
+
+    public void pause(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up *0);
+    }
+
+
+    public void reverse(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * -50);
+    }
+
+
+
+
+    public void start(Collider2D collision)
+    {
+        if (param.mode == -2)
+        {
+            play(collision);
+        }
+
+    }
+
+
+    public void startAction(Collider2D collision)
+    {
+        if (param.mode == 0)
+        {
+            pause(collision);
+        }
+        else if (param.mode == -1)
+        {
+            reverse(collision);
+        }
+        else
+        {
+            play(collision);
+        }
+
+
     }
 
 
